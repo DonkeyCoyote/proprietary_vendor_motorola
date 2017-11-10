64cc80f045ed516436eb44a24aade3cb86edd667
Nikit Sharma <nikitsharma92@gmail.com>
2017-11-10 07:40:43 +0530

    athene: Ship N libgui and libui * Renamed and haxxed to libsu and libsui

--- athene/athene-vendor.mk
+++ athene/athene-vendor.mk
@@ -218,6 +218,8 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/athene/proprietary/lib/libloc_api_v02.so:system/lib/libloc_api_v02.so \
     vendor/motorola/athene/proprietary/lib/libloc_ds_api.so:system/lib/libloc_ds_api.so \
     vendor/motorola/athene/proprietary/lib/libbson.so:system/lib/libbson.so \
+    vendor/motorola/athene/proprietary/vendor/lib/libsui.so:system/vendor/lib/libsui.so \
+    vendor/motorola/athene/proprietary/vendor/lib/libsu.so:system/vendor/lib/libsu.so \
     vendor/motorola/athene/proprietary/lib/libmbs.so:system/lib/libmbs.so \
     vendor/motorola/athene/proprietary/lib/libmdmcutback.so:system/lib/libmdmcutback.so \
     vendor/motorola/athene/proprietary/lib/libminui.so:system/lib/libminui.so \
